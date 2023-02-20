34cbfec4210fa887db6194f116c0abc95bd1d62e
2001adityapatil <89129745+2001adityapatil@users.noreply.github.com>
2023-02-20 16:35:06 +0530

    Update 51. N-Queens.cpp

--- 51. N-Queens.cpp
+++ 51. N-Queens.cpp
@@ -1,4 +1,49 @@
+void nqueens(int col, vector<string> &board, vector<vector<string>> &ans, int n, vector<int> &samerow, vector<int> &lowerdiagonal, vector<int> &upperdiagonal)
+{
+	if(col == n)
+	{
+		ans.push_back(board);
+		return;
+	}
+	
+	for(int row  = 0; row < n; row++)
+	{
+		if(samerow[row] == 0 && lowerdiagonal[row+col] == 0 && upperdiagonal[n-1+col - row] == 0)
+		{
+			board[row][col] = 'Q';
+			samerow[row] = 1;
+			lowerdiagonal[row+col] = 1;
+			upperdiagonal[n-1+col - row] = 1;
+			nqueens(col+1, board, ans, n,  samerow, lowerdiagonal, upperdiagonal);
+			board[row][col] = '.';
+			samerow[row] = 0;
+			lowerdiagonal[row+col] = 0;
+			upperdiagonal[n-1+col - row] = 0;
+		}
+	}
+}
 
+
+
+int main()
+{
+	int n = 4;
+	
+	vector<vector<string>> ans;
+	vector<string> board(n);
+	string s(n,'.');
+	
+	
+	for(int i = 0; i < n; i++)
+		board[i] = s;
+		
+	vector<int> samerow(n,0), upperdiagonal(2*n-1, 0), lowerdiagonal(2*n-1,0);
+	nqueens(0, board, ans, n, samerow, lowerdiagonal, upperdiagonal);
+
+	cout<<ans.size();
+	
+	return 0;
+}
 // extra time complexity .... O(n) + O(n) + O(n)
 class Solution {
     bool solve(int row, int col, vector<string> board, int n)
