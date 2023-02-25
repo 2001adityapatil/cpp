9991a9720b3cd421411c2421ce297254f1b6cc2a
2001adityapatil <89129745+2001adityapatil@users.noreply.github.com>
2023-02-25 10:01:01 +0530

    Update 387. First Unique Character in a String.cpp

--- 387. First Unique Character in a String.cpp
+++ 387. First Unique Character in a String.cpp
@@ -1,19 +1,19 @@
-class Solution {
-public:
-    int firstUniqChar(string s) {
+// class Solution {
+// public:
+//     int firstUniqChar(string s) {
 
-    map<char, int> mp;
-     for(int i = 0; i < s.length(); i++)
-        mp[s[i]]++;
+//     map<char, int> mp;
+//      for(int i = 0; i < s.length(); i++)
+//         mp[s[i]]++;
 
-    for(int i = 0; i < s.length(); i++)
-    {
-        if(mp[s[i]] == 1)
-        return i;
-    }
-    return -1;
-    }
-};
+//     for(int i = 0; i < s.length(); i++)
+//     {
+//         if(mp[s[i]] == 1)
+//         return i;
+//     }
+//     return -1;
+//     }
+// };
 
 
 class Solution {
