96289c57d54c9f10d3f5ec70ebf8e30253b5ce3f
2001adityapatil <89129745+2001adityapatil@users.noreply.github.com>
2023-03-01 17:22:25 +0530

    Update 257. Binary Tree Paths.cpp

--- 257. Binary Tree Paths.cpp
+++ 257. Binary Tree Paths.cpp
@@ -1,3 +1,9 @@
+// Solution: creating two arrays bcoz if root val is 2digits makes a problem in r code call the preorder traversal as it is a preorder traversal push root node val in
+// res vector checking if root node reaches last node or not if reach then extract all the string that are push in res vector and creat the string loop it n-1 times then
+// last value u have to append or if n times then pop 2 values from string and push into ans vector for the remaining it will first go in left of binary tree till root 
+// is null once it reaches it will add the string after it will check in right of root and pop last value from vector if there is value in right it will in its left and
+// right and will all string in ans vec as it reach to null after coming from right recursion it will pop the value main logic thats how we traverse through all nodes
+
 class Solution {
 public:
     vector<string> binaryTreePaths(TreeNode* root) {
