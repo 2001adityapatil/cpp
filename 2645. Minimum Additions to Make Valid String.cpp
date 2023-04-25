9d5345975ad1ef0c549ae482de728795b8cc9a30
2001adityapatil <89129745+2001adityapatil@users.noreply.github.com>
2023-04-26 01:14:40 +0530

    Create 2645. Minimum Additions to Make Valid String.cpp

---
+++ 2645. Minimum Additions to Make Valid String.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int addMinimum(string word) {
+    int n = word.size();
+    int i =  0; 
+    int res = 0;
+
+    while(i < n)
+    {
+        int count = 0;
+
+        if(word[i] == 'a')
+        {
+            count++;
+            i++;
+        }
+
+        if(i < n && word[i] == 'b')
+        {
+            count++;
+            i++;
+        }
+
+        if(i < n && word[i] == 'c')
+        {
+            count++;
+            i++;
+        }
+
+        res +=  3 - count;
+    }
+    return res;
+    }
+};
